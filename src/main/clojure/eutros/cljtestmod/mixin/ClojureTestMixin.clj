364cd16edb3c74c2052dccbdb46b417c9b6e7099
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-07-17 03:08:21 +0100

    Hit the cursed formatter.

--- src/main/clojure/eutros/cljtestmod/mixin/ClojureTestMixin.clj
+++ src/main/clojure/eutros/cljtestmod/mixin/ClojureTestMixin.clj
@@ -5,12 +5,15 @@
            org.spongepowered.asm.mixin.injection.Inject))
 
 (gen-class
- :name    ^{Mixin [TitleScreen]}
-          eutros.cljtestmod.mixin.ClojureTestMixin
- :main    false
- :impl-ns eutros.cljtestmod.internal.ClojureTestMixin
+ :name         ^{Mixin [TitleScreen]}
+               eutros.cljtestmod.mixin.ClojureTestMixin
+ :main         false
+ :impl-ns      eutros.cljtestmod.internal.ClojureTestMixin
  :load-impl-ns false
  :methods
-          [[^{Inject   {:at     (At "HEAD")
-                        :method "init()V"}} ;; FIXME this should generate private and/or static methods
-            init [org.spongepowered.asm.mixin.injection.callback.CallbackInfo] void]])
+               [[^{Inject {:at     (At "HEAD")
+                           :method "init()V"}}
+                 ;; FIXME this should generate private and/or static methods
+                 init
+                 [org.spongepowered.asm.mixin.injection.callback.CallbackInfo]
+                 void]])
